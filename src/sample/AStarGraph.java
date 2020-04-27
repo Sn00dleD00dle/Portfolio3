c3e108fa3a49868927d0ae44235fc408f96a8647
Sn00dleD00dle <61308290+Sn00dleD00dle@users.noreply.github.com>
2020-04-27 11:25:33 +0200

    Added stuff

---
+++ src/sample/AStarGraph.java
@@ -0,0 +1,136 @@
+package sample;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class AStarGraph {
+    private ArrayList<Vertex> vertices;
+
+    public AStarGraph() {
+        vertices = new ArrayList<Vertex>();
+    }
+
+    public void addvertex(Vertex v) {
+        vertices.add(v);
+    }
+
+    public void newconnection(Vertex v1, Vertex v2, Double dist) {
+        v1.addOutEdge(v2, dist);
+        v2.addOutEdge(v1, dist);
+    }
+
+    public boolean A_Star(Vertex start, Vertex destination) {
+        if (start == null || destination == null)
+            return false;
+        PriorityQueue<Vertex> Openlist = new PriorityQueue<Vertex>();
+        ArrayList<Vertex> Closedlist = new ArrayList<Vertex>();
+        Openlist.offer(start);
+        Vertex Current;
+        ArrayList<Vertex> CurrentNeighbors;
+        Vertex Neighbor;
+        //Initialize h with chosen heuristic
+        for (int i = 0; i < vertices.size(); i++) {
+            vertices.get(i).seth(Manhattan(vertices.get(i), destination));
+        }
+        Openlist.remove(start);
+        start.setg(0.0);
+        start.calculatef();
+        Openlist.offer(start);
+        //Start algorithm
+        System.out.println("Start Algorithm");
+        while (!Openlist.isEmpty()) {
+            Current = Openlist.remove();
+            if (Current == destination) {
+                return true;
+            }
+            Closedlist.add(Current);
+            CurrentNeighbors = Current.getNeighbours();
+            for (int v = 0; v < CurrentNeighbors.size(); v++) {
+                Neighbor = CurrentNeighbors.get(v);
+                double tempgofv = Current.getg() + Current.getNeighbourDistance().get(v);
+                if (tempgofv < Neighbor.getg()) {
+                    Neighbor.setPrev(Current);
+                    Neighbor.setg(tempgofv);
+                    Neighbor.calculatef();
+                    if (!Openlist.contains(Neighbor) && !Closedlist.contains(Neighbor)) {
+                        Openlist.offer(Neighbor);
+                    } else if (Openlist.contains(Neighbor)){
+                        Openlist.remove(Neighbor);
+                        Openlist.offer(Neighbor);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public Double Manhattan(Vertex from,Vertex goal){
+        double distance = Math.abs(goal.getx() -from.getx()) + Math.abs(goal.gety() - from.gety());
+        return distance;
+    }
+
+    public Double Euclidean( Vertex from,Vertex to){
+        double squaredX = (to.getx() - from.getx());
+        double squaredY = (to.gety() - from.gety());
+        double distance = Math.sqrt((squaredX * squaredX) + (squaredY * squaredY));
+        return distance;
+    }
+}
+
+class Vertex implements Comparable<Vertex>{
+    private String id;
+    private ArrayList<Vertex> Neighbours=new ArrayList<Vertex>();
+    private ArrayList<Double> NeighbourDistance =new ArrayList<Double>();
+    private Double f;
+    private Double g;
+    private Double h;
+    private Integer x;
+    private Integer y;
+    private Vertex prev =null;
+    public Vertex(String id, int x_cor,int y_cor){
+        this.id=id;
+        this.x=x_cor;
+        this.y = y_cor;
+        f=Double.POSITIVE_INFINITY;
+        g=Double.POSITIVE_INFINITY;
+        h=0.0;
+    }
+
+    public void addOutEdge(Vertex toV, Double dist){
+        Neighbours.add(toV);
+        NeighbourDistance.add(dist);
+    }
+
+    public ArrayList<Vertex> getNeighbours(){
+        return Neighbours;
+    }
+    public ArrayList<Double> getNeighbourDistance(){
+        return NeighbourDistance;
+    }
+    public String getid(){ return id;};
+    public Integer getx(){ return x; }
+    public Integer gety(){return y; }
+    public Double getf() { return f; }
+    public void calculatef(){ f=g+h; }
+
+    public Double getg() { return g; }
+
+    public void setg(Double newg){ g=newg; }
+    public Double geth(){ return h; }
+    public void seth(Double estimate){ h=estimate; }
+    public Vertex getPrev() { return prev; }
+    public void setPrev(Vertex v){prev=v;}
+
+    public void printVertex(){
+        System.out.println(id + " g: "+g+ ", h: "+h+", f: "+f);
+    }
+    @Override
+    public int compareTo(Vertex o) {
+        if (this.getf() > o.getf())
+            return 1;
+        if (this.getf() < o.getf())
+            return -1;
+        return 0;
+    }
+}
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,8 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        AStarGraph graphmodel = createGraph();
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("AStar Graph");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
@@ -20,4 +21,44 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    public AStarGraph createGraph() {
+        AStarGraph MyMaze = new AStarGraph();
+        Vertex A = new Vertex("A", 0, 4);
+        Vertex B = new Vertex("B", 1, 7);
+        Vertex C = new Vertex("C", 4, 0);
+        Vertex D = new Vertex("D", 3, 7);
+        Vertex E = new Vertex("E", 3, 3);
+        Vertex F = new Vertex("F", 6, 6);
+        Vertex G = new Vertex("G", 7, 2);
+        Vertex H = new Vertex("H", 8, 7);
+        Vertex I = new Vertex("I", 9, 2);
+        Vertex J = new Vertex("J", 11, 5);
+
+        MyMaze.addvertex(A);
+        MyMaze.addvertex(B);
+        MyMaze.addvertex(C);
+        MyMaze.addvertex(D);
+        MyMaze.addvertex(E);
+        MyMaze.addvertex(F);
+        MyMaze.addvertex(G);
+        MyMaze.addvertex(H);
+        MyMaze.addvertex(I);
+        MyMaze.addvertex(J);
+        MyMaze.newconnection(A, B, 3.41);
+        MyMaze.newconnection(A, C, 6.82);
+        MyMaze.newconnection(B, D, 2.00);
+        MyMaze.newconnection(C, G, 4.41);
+        MyMaze.newconnection(C, I, 4.82);
+        MyMaze.newconnection(D, E, 4.00);
+        MyMaze.newconnection(E, F, 6.23);
+        MyMaze.newconnection(F, G, 4.41);
+        MyMaze.newconnection(F, H, 3.82);
+        MyMaze.newconnection(G, H, 5.41);
+        MyMaze.newconnection(G, I, 2.82);
+        MyMaze.newconnection(H, J, 4.41);
+        MyMaze.newconnection(I, J, 3.82);
+        return AStarGraph;
+    }
+
 }
